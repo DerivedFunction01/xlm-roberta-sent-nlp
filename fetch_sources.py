893d6d9b07353f25d64b92b8d7bbefd09b476956
Derived <ytdli08@gmail.com>
2026-04-17 09:48:13 -0400

    update

--- fetch_sources.py
+++ fetch_sources.py
@@ -23,7 +23,7 @@ _maybe_login()
 #%%
 def refresh_sources() -> None:
     #%%
-    workers = max(1, mp.cpu_count() // 2)
+    workers = max(1, mp.cpu_count() - 1)
     #%%
     print("Refreshing wiki caches ...")
     _ = load_wiki_sentences(
